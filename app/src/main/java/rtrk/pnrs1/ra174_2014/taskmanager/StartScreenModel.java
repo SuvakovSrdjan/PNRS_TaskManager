6decaf62b8c2b4d5d5b0d81e48f1206af6b7a6f2
Srdjan Suvakov <suvakov.srdjan@gmail.com>
2017-03-27 23:56:58 +0200

    Main Activity Layout done, that is all

---
+++ app/src/main/java/rtrk/pnrs1/ra174_2014/taskmanager/StartScreenModel.java
@@ -0,0 +1,4 @@
+package rtrk.pnrs1.ra174_2014.taskmanager;
+
+public class StartScreenModel {
+}
---
+++ app/src/main/java/rtrk/pnrs1/ra174_2014/taskmanager/StartScreenPresenter.java
@@ -0,0 +1,8 @@
+package rtrk.pnrs1.ra174_2014.taskmanager;
+
+/**
+ * Created by airjetsrka on 3/27/17.
+ */
+
+public class StartScreenPresenter {
+}
